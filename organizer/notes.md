827d8fb03a2d7be05e98711bb240636b57470cfc
Kenny Weiss <kenny1weiss@aol.com>
2023-10-23 12:44:17 -0700

    Access todo app

---
+++ organizer/notes.md
@@ -0,0 +1,2 @@
+Create todo list
+
--- organizer/todo/views.py
+++ organizer/todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def todo_main(request):
+    return HttpResponse("Todo List")
+
+def home(request):
+    return HttpResponse("Home")
