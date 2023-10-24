09cec0c1612b39812b4056f71c02c29776026090
Kenny Weiss <kenny1weiss@aol.com>
2023-10-24 10:35:49 -0700

    create task model for database

--- organizer/todo/models.py
+++ organizer/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    duration = models.BigIntegerField
+    finished = bool
+    worked_on = bool
+    content = models.TextField()
+    notes = models.TextField()
+    date_posted = models.DateTimeField(default)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
